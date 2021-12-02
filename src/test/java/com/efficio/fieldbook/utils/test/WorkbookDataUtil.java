33b4d6e9a8eefdc036c0768cde2f0a701f4a5a2f
leonel@leafnode.io <leonelkovacic@gmail.com>
2021-12-02 17:11:19 -0300

    IBP-5221: implement hibernate association for province in Location

--- src/test/java/com/efficio/fieldbook/utils/test/WorkbookDataUtil.java
+++ src/test/java/com/efficio/fieldbook/utils/test/WorkbookDataUtil.java
@@ -601,10 +601,10 @@ public class WorkbookDataUtil {
 	public static List<Location> createLocationData() {
 		final List<Location> locations = new ArrayList<Location>();
 		locations.add(new Location(WorkbookDataUtil.LOCATION_ID_1, WorkbookDataUtil.LTYPE, WorkbookDataUtil.NLLP, WorkbookDataUtil.LNAME
-				+ " 1", WorkbookDataUtil.LABBR, WorkbookDataUtil.SNL3ID, WorkbookDataUtil.SNL2ID, WorkbookDataUtil.SNL1ID,
+				+ " 1", WorkbookDataUtil.LABBR, WorkbookDataUtil.SNL3ID, WorkbookDataUtil.SNL2ID, new Location(WorkbookDataUtil.SNL1ID),
 				new Location(WorkbookDataUtil.CNTRYID), WorkbookDataUtil.LRPLCE));
 		locations.add(new Location(WorkbookDataUtil.LOCATION_ID_2, WorkbookDataUtil.LTYPE, WorkbookDataUtil.NLLP, WorkbookDataUtil.LNAME
-				+ " 2", WorkbookDataUtil.LABBR, WorkbookDataUtil.SNL3ID, WorkbookDataUtil.SNL2ID, WorkbookDataUtil.SNL1ID,
+				+ " 2", WorkbookDataUtil.LABBR, WorkbookDataUtil.SNL3ID, WorkbookDataUtil.SNL2ID, new Location(WorkbookDataUtil.SNL1ID),
 				new Location(WorkbookDataUtil.CNTRYID), WorkbookDataUtil.LRPLCE));
 		return locations;
 	}
