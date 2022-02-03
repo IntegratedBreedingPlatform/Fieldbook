fdb2edc36c1fc105860e6acac4463ff470e0f72b
cheryleigh <chery@leafnode.io>
2022-02-03 11:42:46 +0800

    IBP-5131: add data-test attributes for plot visualization

--- src/main/webapp/WEB-INF/static/js/trialmanager/subobservations/subObservationSet.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/subobservations/subObservationSet.js
@@ -979,7 +979,7 @@
 						'<"row"<"col-sm-12 paginate-float-center"<"pull-left"i><"pull-right"l>p>>')
 					.withButtons([{
 						className: 'fbk-buttons-no-border fbk-colvis-button',
-						text: '<img src="/Fieldbook/static/img/statistics.svg" height="16px" width="16px"></img>',
+						text: '<img src="/Fieldbook/static/img/statistics.svg" height="16px" width="16px" data-test="plotVisualizationButton"></img>',
 						action: function (e, dt, node, config) {
 
 							var observationUnitsSearch = {
