83b69a808320afc4e6239b9cc4cc9744521d7fde
mderamos <marc.ramos@leafnode.io>
2022-04-18 12:15:14 +0800

    Fixed UI Issue
    IBP-5490

--- src/main/webapp/WEB-INF/static/js/trialmanager/environments.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/environments.js
@@ -181,6 +181,13 @@
 				$scope.showFiles(instanceId, variableName);
 			};
 
+			$scope.showFileIcon = function (instance, settingVariable) {
+				return $scope.isFileStorageConfigured
+					&& instance.fileVariableIds
+					&& instance.fileVariableIds.length
+					&& instance.fileVariableIds.includes(settingVariable.variable.cvTermId.toString());
+			}
+
 			$scope.renderDisplayValue = function (settingVariable, value) {
 
 				return renderByDataType(settingVariable, value);
