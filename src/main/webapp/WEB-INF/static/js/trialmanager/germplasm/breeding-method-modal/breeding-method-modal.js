74291a0f46afe33a163dba513322854da5658527
cheryleigh <chery@leafnode.io>
2022-09-08 15:39:42 +0800

    IBP-5872: add breeding method modal on click of field in germplasm and checks

---
+++ src/main/webapp/WEB-INF/static/js/trialmanager/germplasm/breeding-method-modal/breeding-method-modal.js
@@ -0,0 +1,32 @@
+(function () {
+	'use strict';
+
+	const breedingMethodModule = angular.module('breedingMethodModule', ['ui.bootstrap']);
+	breedingMethodModule.factory('breedingMethodModalService', ['$uibModal', '$rootScope', function ($uibModal, $rootScope) {
+		const breedingMethodModalService = {};
+		breedingMethodModalService.openBreedingMethodModal = function (breedingMethodId) {
+			breedingMethodModalService.modal = $uibModal.open({
+				windowClass: 'modal-large',
+				templateUrl: '/Fieldbook/static/js/trialmanager/germplasm/breeding-method-modal/breeding-method-modal.html',
+				controller: function ($scope, $uibModalInstance, breedingMethodModalService, studyContext) {
+
+					const url = '/ibpworkbench/main/app/#/breeding-method-page/' + breedingMethodId + '?cropName=' + studyContext.cropName
+						+ '&programUUID=' + studyContext.programId
+						+ '&modal=true';
+					$scope.url = url;
+					$scope.breedingMethodId = breedingMethodId;
+
+					window.closeModal = function() {
+						$uibModalInstance.close(null);
+					};
+
+					$scope.clear = function() {
+						$uibModalInstance.close(null);
+					};
+				},
+			}).result;
+		};
+
+		return breedingMethodModalService;
+	}]);
+})();
