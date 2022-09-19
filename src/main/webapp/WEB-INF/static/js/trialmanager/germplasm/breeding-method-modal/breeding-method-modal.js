28181865dffe8a2fe033c294ed392c692544e76d
cheryleigh <chery@leafnode.io>
2022-09-19 11:35:53 +0800

    rename variables, remove unused variable
    IBP-5872

--- src/main/webapp/WEB-INF/static/js/trialmanager/germplasm/breeding-method-modal/breeding-method-modal.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/germplasm/breeding-method-modal/breeding-method-modal.js
@@ -4,17 +4,16 @@
 	const breedingMethodModule = angular.module('manageTrialApp');
 	breedingMethodModule.factory('breedingMethodModalService', ['$uibModal', '$rootScope', function ($uibModal, $rootScope) {
 		const breedingMethodModalService = {};
-		breedingMethodModalService.openBreedingMethodModal = function (breedingMethodId) {
+		breedingMethodModalService.openBreedingMethodModal = function (breedingMethodAbbr) {
 			breedingMethodModalService.modal = $uibModal.open({
 				windowClass: 'modal-large',
 				templateUrl: '/Fieldbook/static/js/trialmanager/germplasm/breeding-method-modal/breeding-method-modal.html',
 				controller: function ($scope, $uibModalInstance, breedingMethodModalService, studyContext) {
 
-					const url = '/ibpworkbench/main/app/#/breeding-method-page/' + breedingMethodId + '?cropName=' + studyContext.cropName
+					const url = '/ibpworkbench/main/app/#/breeding-method-page/' + breedingMethodAbbr + '?cropName=' + studyContext.cropName
 						+ '&programUUID=' + studyContext.programId
 						+ '&modal=true';
 					$scope.url = url;
-					$scope.breedingMethodId = breedingMethodId;
 
 					window.closeModal = function() {
 						$uibModalInstance.close(null);
