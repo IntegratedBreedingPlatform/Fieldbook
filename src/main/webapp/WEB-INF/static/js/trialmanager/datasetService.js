12aaa1154a5b3ee67efd968f0634b2ab212f3bcd
cuenyad <cuenya.diego@gmail.com>
2022-11-04 16:19:38 -0300

    Adding services to get the properties of the dataset.
    Issue: IBP-5778/IBP-5983
    Review: none

--- src/main/webapp/WEB-INF/static/js/trialmanager/datasetService.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/datasetService.js
@@ -126,6 +126,11 @@
 				return request.then(successHandler, failureHandler);
 			};
 
+			datasetService.getAllProperties = function (datasetId, variableTypeId) {
+				var request = $http.get(BASE_URL + studyContext.studyId + '/datasets/' + datasetId + '/properties/');
+				return request.then(successHandler, failureHandler);
+			};
+
 			datasetService.removeVariables = function (datasetId, variableIds) {
 				var request = $http.delete(BASE_URL + studyContext.studyId + '/datasets/' + datasetId + '/variables?', {
 					params: {
