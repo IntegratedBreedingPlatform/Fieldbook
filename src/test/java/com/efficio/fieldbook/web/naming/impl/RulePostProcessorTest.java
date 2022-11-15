aedba8fbabf609cc7daf9df90720d654c6ab74b0
leonel@leafnode.io <leonelkovacic@gmail.com>
2022-11-15 13:58:16 -0300

    IBP-5941: revert moving classes to a new package

--- src/test/java/com/efficio/fieldbook/web/naming/impl/RulePostProcessorTest.java
+++ src/test/java/com/efficio/fieldbook/web/naming/impl/RulePostProcessorTest.java
@@ -13,7 +13,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.ContextConfiguration;
 
-import org.generationcp.middleware.ruleengine.naming.newrules.CountRule;
+import org.generationcp.middleware.ruleengine.naming.rules.CountRule;
 import com.efficio.fieldbook.web.trial.controller.CreateTrialController;
 
 /**
