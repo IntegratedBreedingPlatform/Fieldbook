23ec0aec7834e11ecd4f1c4efae0c3211a834d00
cuenyad <cuenya.diego@gmail.com>
2023-03-15 10:42:09 -0300

    Implements permissions to export Design Template.
    Issue: IBP-5924/IBP-6238
    Review: none

--- src/main/webapp/WEB-INF/static/js/trialmanager/manageTrial.js
+++ src/main/webapp/WEB-INF/static/js/trialmanager/manageTrial.js
@@ -630,6 +630,10 @@ showAlertMessage,showMeasurementsPreview,createErrorNotification,errorMsgHeader,
 				return $scope.hasDesignGenerated && HasAnyAuthorityService.hasAnyAuthority(PERMISSIONS.CREATE_PLANTING_LABELS_PERMISSIONS);
 			}
 
+			$scope.showExportDesignTemplateAction = function () {
+				return HasAnyAuthorityService.hasAnyAuthority(PERMISSIONS.EXPORT_DESIGN_TEMPLATE_PERMISSIONS);
+			}
+
 			$scope.showDesignAndPlanningOptions = function () {
 				return $scope.showPreparePlantingInventoryAction() || $scope.hasManageStudiesPermission;
 			}
