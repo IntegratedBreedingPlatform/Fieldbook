295a4e3bc3a1f8f02e671e5697fc3f65b33d840e
Aldrin Batac <aldrin.batac@leafnode.io>
2023-03-17 07:22:17 +0800

    IBP-6188 / IBP-6236 - Add angularjs sampleListService.

---
+++ src/main/webapp/WEB-INF/static/js/trialmanager/sample-lists/sampleListService.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    var module = angular.module('manageTrialApp');
+
+    module.factory('sampleListService', ['$http', '$q', 'studyContext', 'serviceUtilities',
+        function ($http, $q, studyContext, serviceUtilities) {
+
+            var BASE_URL = '/bmsapi/crops/' + studyContext.cropName + '/programs/' + studyContext.programId;
+
+            var successHandler = serviceUtilities.restSuccessHandler,
+                failureHandler = serviceUtilities.restFailureHandler;
+
+            var sampleListService = {};
+
+            sampleListService.getSampleListsInStudy = function (withGenotypesOnly) {
+                var request = $http.get(BASE_URL + '/sample-lists/' + studyContext.studyId + '?withGenotypesOnly=' + withGenotypesOnly);
+                return request.then(successHandler, failureHandler);
+            };
+
+            return sampleListService;
+
+        }]);
+
+})();
